faf1f6f2a6bfea07c944320a35b1399460268dd9
adadesions <adadesions@gmail.com>
2022-04-02 13:10:20 +0700

    Finish EP7

---
+++ pages/index.vue
@@ -0,0 +1,97 @@
+<template>
+  <v-container>
+    <v-row class="border" justify="center">
+      <h3>AdaDeSions</h3>
+    </v-row>
+
+    <v-row class="profile" justify="space-around">
+      <div class="avatar">
+        <v-avatar>
+          <img src="https://avatars.githubusercontent.com/u/3336816?v=4" alt="Ada" />
+        </v-avatar>
+      </div>
+
+      <!-- Post Text-box -->
+      <div class="text-box">
+        <div class="text-center">
+          1,992 <br />
+          Post
+        </div>
+      </div>
+
+      <!-- Follower Text-box -->
+      <div class="text-box">
+        <div class="text-center">
+          2.3M <br />
+          Followers
+        </div>
+      </div>
+
+      <!-- Following Text-box -->
+      <div class="text-box">
+        <div class="text-center">
+          1.5M <br />
+          Following
+        </div>
+      </div>
+    </v-row>
+
+    <v-row class="bio">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </v-row>
+
+    <v-row class="margin-top" justify="space-around">
+      <v-btn elevation="7" large>Follow</v-btn>
+      <v-btn elevation="7" large>Message</v-btn>
+      <v-btn elevation="7" large>
+        <v-icon>mdi-chevron-down</v-icon>
+      </v-btn>
+    </v-row>
+    <v-row class="margin-top">
+      <v-tabs grow>
+        <v-tab>
+          <v-icon>mdi-grid</v-icon>
+        </v-tab>
+        <v-tab>
+          <v-icon>
+            mdi-tag-heart-outline
+          </v-icon>
+        </v-tab>        
+      </v-tabs>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
+
+<style scoped>
+.border {
+  border: 1px solid #fff;
+}
+
+.profile {
+  margin-top: 3em;
+}
+
+.bio {
+  margin-top: 3em;
+  margin-left: 0.7em;
+  font-size: 12px;
+}
+
+.margin-top {
+  margin-top: 3em;
+}
+</style>
