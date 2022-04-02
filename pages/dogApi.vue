818bc65d597c235b72461c89d702533e46d4d014
adadesions <adadesions@gmail.com>
2022-04-02 14:04:56 +0700

    Show dog imgs

---
+++ pages/dogApi.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="dog-api-section">
+      <v-row>
+          <div class="dog-imgs d-flex" v-for="(img, idx) in dogData" :key="idx">
+              <v-img :src="img" max-width="100"></v-img>
+          </div> 
+      </v-row>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: "dogApi",
+    data: () => ({
+        dogData: [],
+    }),
+    async mounted() {
+        try {
+            this.dogData = await axios.get("https://dog.ceo/api/breed/pomeranian/images")
+            this.dogData = this.dogData.data.message
+        }
+        catch(err) {
+            this.dogData = "Data Not Found"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
